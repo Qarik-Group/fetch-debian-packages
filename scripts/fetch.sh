fb2bf1aa817a90d099a381bae346a30b7195f876
Dr Nic Williams <drnicwilliams@gmail.com>
2018-03-28 09:54:13 +1000

    include all the trust bosh packags in image so they aren't refetched

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -9,7 +9,6 @@ cacheDir="$baseDir/apt/cache"
 stateDir="$baseDir/apt/state"
 options="-o debug::nolocking=true -o dir::cache=$cacheDir -o dir::state=$stateDir -o dir::etc::sourcelist=$sourceList -o dir::etc::trusted=$trustedKeys"
 
-set -x
 mkdir -p "$(dirname $sourceList)"
 mkdir -p "$(dirname $trustedKeys)"
 mkdir -p "$cacheDir"
@@ -22,8 +21,19 @@ if [[ ! -f /apt-config/apt.json ]]; then
   packages=$@
   echo "No /apt-config/apt.json found, looking for: $packages"
 else
+  keys=$(jq -r ".keys[]" /apt-config/apt.json)
+  repos=$(jq -r ".repos[]" /apt-config/apt.json)
   packages=$(jq -r ".packages[]" /apt-config/apt.json)
 fi
 
-apt-get update ${options}
-apt-get install ${options} -y $packages
+for keyURL in "${keys[@]}"; do
+  curl -L $keyURL | apt-key --keyring $trustedKeys adv -
+done
+
+for repo in "${repos[@]}"; do
+  echo "$repo" >> $sourceList
+done
+
+set -x
+# apt-get update ${options}
+# apt-get install ${options} -y $packages
